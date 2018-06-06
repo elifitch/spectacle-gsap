52b95787d131db20aa0ce01eb2c5ba605a2f999b
Eli Fitch <eli.fitch.prof@gmail.com>
2018-06-05 23:28:32 -0400

    deploying demo

--- demo/components/inline-block.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import styled from 'react-emotion';
-
-const InlineBlock = styled('div')`
-  display: inline-block;
-`;
-
-export default InlineBlock;
